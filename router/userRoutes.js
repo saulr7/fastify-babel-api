e9f838b9313151ea6827f7460ca58f2d51fbcd25
Saul Ramirez <fernando.ramire@hotmail.com>
2022-02-03 09:47:24 -0600

    :art: using async instead of explicit response

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -2,13 +2,16 @@ const handlerV2 = (req, res) => {
   res.send({ ok: true, msg: 'It works fuckers' });
 };
 
-const handlerV3 = (req, res) => {
+const handlerV3 = async (req) => {
   const { id } = req.params;
-  res.send({ ok: true, msg: `It works fuckers 3${id}` });
+  return { ok: true, msg: `It works fuckers 3${id}` };
 };
+
+const createUser = async (req) => ({ ok: true, msg: 'It works fuckers 3', body: req.body });
 
 const userRoutes = (fastify, opts, done) => {
   fastify.get('/', handlerV2);
+  fastify.post('/', createUser);
   fastify.get('/:id', handlerV3);
   done();
 };
